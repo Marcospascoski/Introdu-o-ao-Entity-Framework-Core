77367cc8a0ceccebd732404882fff176c8ab65fa
marcospascoski <marcospascoski@gmail.com>
2021-06-26 11:49:12 -0400

    Criacao das classes de Domain e valueObjects

---
+++ CursoDidatico/Sistema.Pedidos/Domain/Pedido.cs
@@ -0,0 +1,22 @@
+﻿using Sistema.Pedidos.ValueObjetcs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Pedidos.Domain
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+        public DateTime IniciadoEm { get; set; }
+        public DateTime FinalizadoEm { get; set; }
+        public TipoFrete TipoFrete { get; set; }
+        public StatusPedido status { get; set; }
+        public string Observacao { get; set; }
+        public ICollection<PedidoItem> Itens { get; set; }
+    }
+}
