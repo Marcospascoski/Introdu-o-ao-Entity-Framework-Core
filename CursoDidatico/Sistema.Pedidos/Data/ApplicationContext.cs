41849c7992b239939c15186f0903a8ee92c5eb50
marcospascoski <marcospascoski@gmail.com>
2021-06-26 12:43:54 -0400

    Criacao da Classe DbContext

---
+++ CursoDidatico/Sistema.Pedidos/Data/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Pedidos.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=127.0.0.1,1433;Database=SistemaPedido;User Id=sa;Password=test-123;");
+        }
+    }
+}
